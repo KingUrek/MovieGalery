28d68bad672ff5108bb47511fe23bd8bbdb450ec
Gabriel Lucas <dreadpool38@gmail.com>
2020-04-25 10:20:25 -0300

    Updates

---
+++ precache-manifest.ad4705b5386609d133bc18316f6c04ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b660d16221ab47466496cb407f9a69c",
+    "url": "/MovieGalery/index.html"
+  },
+  {
+    "revision": "b44b0a9568c5cb77e32c",
+    "url": "/MovieGalery/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "845b0f81371d226f3768",
+    "url": "/MovieGalery/static/js/2.a892876b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieGalery/static/js/2.a892876b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44b0a9568c5cb77e32c",
+    "url": "/MovieGalery/static/js/main.ac24fb7f.chunk.js"
+  },
+  {
+    "revision": "8a2ef3f0501618238120",
+    "url": "/MovieGalery/static/js/runtime-main.a73b179c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MovieGalery/static/media/logo.5d5d9eef.svg"
+  }
+]);
